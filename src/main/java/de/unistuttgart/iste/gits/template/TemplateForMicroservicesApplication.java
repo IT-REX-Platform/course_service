e5e322b4deee8984ac8b9636a457640e3945b1d7
PaulBredl <st156256@stud.uni-stuttgart.de>
2023-05-01 11:56:40 +0200

    setup basic string project

---
+++ src/main/java/de/unistuttgart/iste/gits/template/TemplateForMicroservicesApplication.java
@@ -0,0 +1,18 @@
+package de.unistuttgart.iste.gits.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the entry point of the application.
+ * <p>
+ * TODO: Rename the package and the class to match the microservice name.
+ */
+@SpringBootApplication
+public class TemplateForMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateForMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/unistuttgart/iste/gits/template/TemplateForMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.iste.gits.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateForMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
