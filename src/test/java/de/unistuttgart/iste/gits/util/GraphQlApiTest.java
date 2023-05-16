0ae3e4e5d2582c13c47409417d165eef7fded753
PaulBredl <paulbredl@gmx.de>
2023-05-16 12:56:47 +0200

    add test extension to clear database after each test

--- src/test/java/de/unistuttgart/iste/gits/util/GraphQlApiTest.java
+++ src/test/java/de/unistuttgart/iste/gits/util/GraphQlApiTest.java
@@ -3,23 +3,22 @@ package de.unistuttgart.iste.gits.util;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.lang.annotation.*;
 
 /**
- * This annotation can be used to annotate a test class to run it as a graphQL integration test.
+ * This annotation can be used to annotate a test class to run it as a graphQL API test.
  * It will do the following:
  * <ul>
  *     <li>Use a different database than the production database</li>
  *     <li>Drop and recreate the database before and after the test</li>
+ *     <li>Delete all tables after each test</li>
  *     <li>Inject a {@link GraphQlTester} into the test methods, see {@link GraphQlTesterParameterResolver} </li>
  * </ul>
  * <p>
- * Test methods that create or modify data should be annotated with {@link DirtiesContext @DirtiesContext}
- * to ensure that the database is recreated before the next test method is executed.
  */
 @ExtendWith(GraphQlTesterParameterResolver.class)
+@ExtendWith(ClearDatabase.class)
 @SpringBootTest(properties = {
         "spring.jpa.hibernate.ddl-auto=create-drop", // create and drop tables before and after tests
         "spring.datasource.url=jdbc:postgresql://localhost:1033/test_data", // use the test database
