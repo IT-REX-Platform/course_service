5b33c5c588860a0ad789f32015675afa0a10920e
Timothy <tidee98@web.de>
2023-06-20 15:44:39 +0200

    Adding: new Tests

--- src/test/java/de/unistuttgart/iste/gits/course_service/service/ResourceServiceTest.java
+++ src/test/java/de/unistuttgart/iste/gits/course_service/service/ResourceServiceTest.java
@@ -1,18 +1,28 @@
 package de.unistuttgart.iste.gits.course_service.service;
 
+import de.unistuttgart.iste.gits.common.dapr.CourseAssociationDTO;
+import de.unistuttgart.iste.gits.common.dapr.CrudOperation;
+import de.unistuttgart.iste.gits.course_service.persistence.dao.ChapterEntity;
 import de.unistuttgart.iste.gits.course_service.persistence.dao.CourseEntity;
 import de.unistuttgart.iste.gits.course_service.persistence.dao.CourseResourceAssociationEntity;
+import de.unistuttgart.iste.gits.course_service.persistence.repository.ChapterRepository;
 import de.unistuttgart.iste.gits.course_service.persistence.repository.CourseRepository;
 import de.unistuttgart.iste.gits.course_service.persistence.repository.ResourceRepository;
+import de.unistuttgart.iste.gits.generated.dto.Course;
 import de.unistuttgart.iste.gits.generated.dto.CourseResourceAssociation;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -21,7 +31,9 @@ class ResourceServiceTest {
     private final ResourceRepository resourceRepository = Mockito.mock(ResourceRepository.class);
     private final CourseRepository courseRepository = Mockito.mock(CourseRepository.class);
 
-    private final ResourceService resourceService = new ResourceService(resourceRepository, courseRepository);
+    private final ChapterRepository chapterRepository = Mockito.mock(ChapterRepository.class);
+
+    private final ResourceService resourceService = new ResourceService(resourceRepository, courseRepository, chapterRepository);
 
     /**
      * This test checks if a correct Resource DTO is created and has the correct availability of a resource for a course depending on the published variable.
@@ -106,6 +118,55 @@ class ResourceServiceTest {
         assertEquals(expected, actualResult.get(0));
     }
 
+    @Test
+    void testResourceCreation(){
+        //init
+        CourseEntity courseEntity = dummyCourseEntityBuilder(OffsetDateTime.now(), true).build();
+        ChapterEntity chapterEntity = dummyChapterEntityBuilder(courseEntity.getId()).build();
+
+        courseEntity.setChapters(List.of(chapterEntity));
+
+        CourseAssociationDTO dto = CourseAssociationDTO.builder().chapterId(chapterEntity.getId()).resourceId(UUID.randomUUID()).operation(CrudOperation.CREATE).build();
+
+
+        //mock repositories
+        when(chapterRepository.findById(any(UUID.class)))
+                .thenReturn(Optional.of(chapterEntity));
+        when(courseRepository.findCourseEntityByChaptersContaining(any(ChapterEntity.class)))
+                .thenReturn(Optional.of(courseEntity));
+
+        //execute
+        assertDoesNotThrow(() -> resourceService.updateResourceAssociations(dto));
+    }
+
+    @Test
+    void testResourceCreationFromInvalidInput(){
+        //init
+        UUID chapterId = UUID.randomUUID();
+        ChapterEntity chapterEntity = dummyChapterEntityBuilder(chapterId).build();
+        CourseAssociationDTO missingDbCourseEntity = CourseAssociationDTO.builder().chapterId(chapterId).resourceId(UUID.randomUUID()).operation(CrudOperation.CREATE).build();
+        CourseAssociationDTO missingDbChapterEntity = CourseAssociationDTO.builder().chapterId(UUID.randomUUID()).resourceId(UUID.randomUUID()).operation(CrudOperation.DELETE).build();
+
+        CourseAssociationDTO missingChapter = CourseAssociationDTO.builder().resourceId(UUID.randomUUID()).operation(CrudOperation.CREATE).build();
+        CourseAssociationDTO missingResource = CourseAssociationDTO.builder().chapterId(UUID.randomUUID()).operation(CrudOperation.CREATE).build();
+        CourseAssociationDTO missingOperator = CourseAssociationDTO.builder().chapterId(UUID.randomUUID()).resourceId(UUID.randomUUID()).build();
+
+        // mock repository
+        when(chapterRepository.findById(chapterId))
+                .thenReturn(Optional.of(chapterEntity));
+
+        // act and assert
+        // case: chapter or course does not exist
+        assertThrows(NoSuchElementException.class, () -> resourceService.updateResourceAssociations(missingDbChapterEntity));
+        assertThrows(NoSuchElementException.class, () -> resourceService.updateResourceAssociations(missingDbCourseEntity));
+
+        // case: incomplete DTO
+        assertThrows(NullPointerException.class, () -> resourceService.updateResourceAssociations(missingChapter));
+        assertThrows(NullPointerException.class, () -> resourceService.updateResourceAssociations(missingResource));
+        assertThrows(NullPointerException.class, () -> resourceService.updateResourceAssociations(missingOperator));
+    }
+
+
     // Builder methods used for creating entities
     private CourseEntity.CourseEntityBuilder dummyCourseEntityBuilder(OffsetDateTime now, boolean published) {
         return CourseEntity.builder()
@@ -127,6 +188,17 @@ class ResourceServiceTest {
                 .endDate(now.minusMonths(2))
                 .published(published);
 
+    }
+
+    private static ChapterEntity.ChapterEntityBuilder dummyChapterEntityBuilder(UUID courseId) {
+        return ChapterEntity.builder()
+                .id(UUID.randomUUID())
+                .courseId(courseId)
+                .title("testTitle")
+                .description("testDescription")
+                .startDate(OffsetDateTime.now())
+                .endDate(OffsetDateTime.now().plusMonths(1))
+                .number(1);
     }
 
 }
